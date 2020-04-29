8b4c50d3fd493f55d7a54a4ad2b148700eb12d71
Andreas <andreas.petersen.3540@gmail.com>
2020-04-29 11:43:04 +0200

    need some code from github

--- src/Admin.js
+++ src/Admin.js
@@ -1,10 +1,68 @@
-import React from "react";
+import React, { useState } from "react";
+import settingUrl from "./settings";
 
 export function Admin() {
+  const [jokeContent, setJokeContent] = useState(<br />);
+  const [createdBy, setCreatedBy] = useState(<br />);
+  const [jokeId, setJokeId] = useState(<br />);
+
+
+  const URL = settingUrl.internalJokes();
+
+  function fetchInternalJokes() {
+    let options = {
+      method: "GET",
+      headers: {
+        "Content-Type": "application/json",
+        Accept: "application/json",
+      },
+    };
+    fetch(URL, options)
+      .then((res) => res.json())
+      .then((data) => {
+        console.log(data);
+        setJokeContent(data.jokeList);
+        setCreatedBy(data.createdBy);
+        setJokeId(data.jokeId);
+      });
+  }
+  function deleteInternalJokes() {
+    let options = {
+      method: "DELETE",
+      headers: {
+        "Content-Type": "application/json",
+        Accept: "application/json",
+      },
+    };
+    fetch(URL, options)
+      .then((res) => res.json())
+      .then((data) => {
+        console.log(data);
+        
+      });
+  }
+
   return (
-  <div>
-    <h1>Admin</h1>
-    <p>This page is only for admins</p>
-  </div>
+    <div>
+      <h1>User Jokes</h1>
+      <button onClick={fetchInternalJokes}>Press to fetch from API's!</button>
+      <p>{jokeContent}</p>
+      <table border="1" width="50%">
+        <thead>
+          <tr>
+            <th>Jokes</th>
+            <th width="150px">Created By</th>
+            <th width="50px"></th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td>{jokeContent}</td>
+            <td>Smith</td>
+            <td align="center"><button onClick={deleteInternalJokes}>DELETE</button></td>
+          </tr>
+        </tbody>
+      </table>
+    </div>
   );
 }

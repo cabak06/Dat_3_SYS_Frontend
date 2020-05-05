428b7f5944b03f033a8146f27b13887f21d06cd4
Cahit <cahit10@hotmail.com>
2020-05-05 14:06:24 +0200

    added sorting/filtermethods to userjokes

--- src/UserJokes.js
+++ src/UserJokes.js
@@ -5,6 +5,7 @@ import facade from "./apiFacade";
 export function UserJokes() {
   const URL = settingUrl.internalJokes();
   const [userJokes, setUserJokes] = useState([]);
+  const [update,setUpdate] = useState(false);
   let options = facade.makeOptions("GET", true);
 
   useEffect(() => {
@@ -15,10 +16,40 @@ export function UserJokes() {
     facade.fetchInternalJokes("/api/joke/userjokes", setUserJokes);
   }
 
+  function sortNewest(){
+        let sortedJokesByID = userJokes.sort((joke1, joke2) =>{
+        return joke2.id - joke1.id;
+    })
+setUserJokes(sortedJokesByID);
+setUpdate(!update);
+
+  }
+
+    function sortByCreator(){
+        let sortedJokes = userJokes.sort((joke1, joke2) =>{
+        return joke1.createdBy.toLowerCase().localeCompare(joke2.createdBy.toLowerCase());
+    })
+setUserJokes(sortedJokes)
+setUpdate(!update);
+}
+
+
+  function sortOldest(){
+        let sortedJokesByID = userJokes.sort((joke1, joke2) =>{
+        return joke1.id - joke2.id;
+    })
+setUserJokes(sortedJokesByID);
+setUpdate(!update);
+}
+ 
   return (
     <div>
       <h1>UserJoke</h1>
       <button onClick={fetchInternalJokes}>Press to fetch from API's!</button>
+      <button onClick={sortNewest}>Sort by Newest</button>
+      <button onClick={sortOldest}>Sort by Oldest</button>
+      <button onClick={sortByCreator}>Sort by User</button>
+     <br/><br/>
       <table border="1" width="50%">
         <thead>
           <tr>
